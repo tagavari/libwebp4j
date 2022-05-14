ac1dd28cf5eb2b4f7ddadc465081997b17fe918b
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-05-14 09:55:18 -0400

    Use Files.createTempFile

--- src/main/java/me/tagavari/libwebp4j/NativeLoader.java
+++ src/main/java/me/tagavari/libwebp4j/NativeLoader.java
@@ -1,17 +1,17 @@
 package me.tagavari.libwebp4j;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 class NativeLoader {
 	private static boolean isLoaded = false;
 
 	private static final String nativeLibraryName = "libwebp4j";
-	private static final String[] nativeLibraryExtensions = {"so", "dll", "dylib"};
+	private static final String[] nativeLibraryExtensions = {".so", ".dll", ".dylib"};
 
 	/**
 	 * Loads native libraries
@@ -23,7 +23,7 @@ class NativeLoader {
 		URL resourceURL = null;
 		String resourceExtension = null;
 		for(String extension : nativeLibraryExtensions) {
-			resourceURL = NativeLoader.class.getClassLoader().getResource(nativeLibraryName + "." + extension);
+			resourceURL = NativeLoader.class.getClassLoader().getResource(nativeLibraryName + extension);
 			if(resourceURL != null) {
 				resourceExtension = extension;
 				break;
@@ -35,19 +35,19 @@ class NativeLoader {
 		}
 
 		//Copy the library to a temporary location
-		File extractedLibrary;
+		Path extractedLibrary;
 		try {
-			extractedLibrary = File.createTempFile(nativeLibraryName, resourceExtension);
-			try(InputStream resourceStream = resourceURL.openStream()) {
-				Files.copy(resourceStream, extractedLibrary.toPath(), StandardCopyOption.REPLACE_EXISTING);
-			}
+			extractedLibrary = Files.createTempFile(nativeLibraryName, resourceExtension);
 			
+			try(InputStream resourceStream = resourceURL.openStream()) {
+				Files.copy(resourceStream, extractedLibrary, StandardCopyOption.REPLACE_EXISTING);
+			}
 		} catch(IOException exception) {
 			throw new RuntimeException("Failed to copy temporary file for native library " + nativeLibraryName, exception);
 		}
 
 		//Load the library
-		System.load(extractedLibrary.getPath());
+		System.load(extractedLibrary.toString());
 
 		isLoaded = true;
 	}
