40fabef5295e88235e0549f38b85c1869cc2eeb1
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-05-14 17:24:07 -0400

    Update libwebp-windows.ps1

--- setup/libwebp-windows.ps1
+++ setup/libwebp-windows.ps1
@@ -5,8 +5,11 @@ $LibVer = $args[0]
 $LibArchive = "libwebp-$LibVer-windows-x64"
 
 #Download and extract the release package
+$DownloadPath = New-TemporaryFile
 $LibArchiveURL = "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/$LibArchive.zip"
-(Invoke-WebRequest $LibArchiveURL).Content | Expand-Archive -DestinationPath $Env:TEMP
+Invoke-WebRequest -OutFile $DownloadPath $LibArchiveURL
+Expand-Archive -Path $DownloadPath -DestinationPath $Env:TEMP
+Remove-Item $DownloadPath
 
 #Add the directory to the PATH
 Add-Content $Env:GITHUB_PATH "$Env:TEMP\$LibArchive\lib"
